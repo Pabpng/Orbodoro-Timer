8685c8d7bc8780ba45f75d3e0fb31f4c01fe13a1
slovakpaul <paulslovak12@gmail.com>
2026-02-06 23:27:04 +1300

    Added Pomodoro, Short-Break, Long-Break selection

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 //Imports
 const start = document.getElementById("start");
-const pause = document.getElementById("pause")
-const reset = document.getElementById("reset")
-const timer = document.getElementById("timer")
+const pause = document.getElementById("pause");
+const reset = document.getElementById("reset");
+const shortBreak = document.getElementById("shortBreak");
+const longBreak = document.getElementById("longBreak");
+const timer = document.getElementById("timer");
+const pomodoroTimer = document.getElementById("pomodoro");
 
 //Timer Logic
-let timeLeft = 25 * 60;
+let timeLeft = 0;
 let interval;
+let selectedTime = 0;
 
 const updateTimer = () => {
     const minutes = Math.floor(timeLeft / 60);
@@ -19,7 +23,11 @@ const updateTimer = () => {
 };
 
 const startTimer = () => {
-    interval = setInterval(() => {
+    if (timeLeft === 0){
+        alert("Time needs to be selected!")
+    } else {
+
+        interval = setInterval(() => {
         timeLeft--;
         updateTimer();
 
@@ -29,9 +37,8 @@ const startTimer = () => {
             timeLeft = 25 * 60;
             updateTimer();
         }
-
     }, 1000)
-
+    }
 };
 
 //Pause Timer Function
@@ -39,7 +46,28 @@ const pauseTimer = () => clearInterval(interval);
 
 const resetTimer = () => {
     clearInterval(interval);
+    timeLeft = selectedTime;
+    updateTimer();
+}
+
+const Pomodoro = () => {
+    clearInterval(interval);
     timeLeft = 25 * 60;
+    selectedTime = 25 * 60;
+    updateTimer();
+}
+
+const shortTimer = () => {
+    clearInterval(interval);
+    timeLeft = 5 * 60;
+    selectedTime = 5 * 60;
+    updateTimer();
+}
+
+const longTimer = () => {
+    clearInterval(interval);
+    timeLeft = 10 * 60;
+    selectedTime = 10 * 60;
     updateTimer();
 }
 
@@ -47,5 +75,8 @@ const resetTimer = () => {
 start.addEventListener("click", startTimer);
 pause.addEventListener("click", pauseTimer);
 reset.addEventListener("click", resetTimer);
+shortBreak.addEventListener("click", shortTimer);
+longBreak.addEventListener("click", longTimer);
+pomodoroTimer.addEventListener("click", Pomodoro);
 
 
